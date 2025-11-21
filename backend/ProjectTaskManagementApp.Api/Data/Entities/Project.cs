49e8608ae2e00e7eb51889a8db99d2d0cc977ef1
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-11-21 01:49:09 +0100

    Backend: add models (Project, TaskItem)

---
+++ backend/ProjectTaskManagementApp.Api/Data/Entities/Project.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTaskManagementApp.Api.Data.Entities
+{
+    public class Project
+    {
+        public Project()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<TaskItem> TaskItems { get; set; } = new List<TaskItem>();
+    }
+}
---
+++ backend/ProjectTaskManagementApp.Api/Data/Entities/TaskItem.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTaskManagementApp.Api.Data.Entities
+{
+    public enum TaskStatus
+    {
+        Todo,
+        InProgress,
+        Done
+    }
+
+    public class TaskItem
+    {
+        public TaskItem()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public Project Project { get; set; } = null!;
+
+        [Required]
+        [MaxLength(150)]
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public TaskStatus Status { get; set; } = TaskStatus.Todo;
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
