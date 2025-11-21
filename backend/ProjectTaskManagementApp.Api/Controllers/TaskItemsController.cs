e5578e81427580d2534293d460aa6cf2da89b5d6
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-11-21 03:08:30 +0100

    Backend: scaffold ProjectsController and TaskItemsController

---
+++ backend/ProjectTaskManagementApp.Api/Controllers/TaskItemsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectTaskManagementApp.Api.Data;
+using ProjectTaskManagementApp.Api.Data.Entities;
+
+namespace ProjectTaskManagementApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TaskItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TaskItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        // GET: api/TaskItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTaskItem(Guid id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            return taskItem;
+        }
+
+        // PUT: api/TaskItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTaskItem(Guid id, TaskItem taskItem)
+        {
+            if (id != taskItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(taskItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TaskItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTaskItem(TaskItem taskItem)
+        {
+            _context.TaskItems.Add(taskItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTaskItem", new { id = taskItem.Id }, taskItem);
+        }
+
+        // DELETE: api/TaskItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTaskItem(Guid id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TaskItems.Remove(taskItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskItemExists(Guid id)
+        {
+            return _context.TaskItems.Any(e => e.Id == id);
+        }
+    }
+}
