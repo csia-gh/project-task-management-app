b531b5352cbb71628e44d806d4cb9b788401216e
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-11-21 02:44:28 +0100

    Backend: enable Swagger for API documentation

--- backend/ProjectTaskManagementApp.Api/Program.cs
+++ backend/ProjectTaskManagementApp.Api/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// OpenAPI + Swagger
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // EF Core + MySQL
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -23,6 +26,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
