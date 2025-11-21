57dba3738944e487ec5236cc42407141f12800b9
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-11-21 11:35:07 +0100

    Frontend: setup routing, pages, and Bootstrap

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProjectList } from './pages/project-list/project-list';
+import { ProjectDetail } from './pages/project-detail/project-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Projects', component: ProjectList },
+  { path: 'projects/:id', title: 'Project Details', component: ProjectDetail },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
