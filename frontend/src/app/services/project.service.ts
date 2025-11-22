a2d5250eff91989ff025a59061b7a790225b5147
Imre Cserhati <cserhati.imre.andras@gmail.com>
2025-11-22 02:29:21 +0100

    Frontend: Add getProjectTasks methods to ProjectService with status filtering

--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { Project } from '../models/project.model';
+import { Observable } from 'rxjs';
+import { TaskItem } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +11,16 @@ import { Project } from '../models/project.model';
 export class ProjectService extends BaseService<Project> {
   constructor(http: HttpClient) {
     super(http, 'projects');
+  }
+
+  getProjectTasks(projectId: string): Observable<TaskItem[]> {
+    return this.http.get<TaskItem[]>(`${this.baseUrl}/projects/${projectId}/tasks`);
+  }
+
+  // GET /api/projects/{projectId}/tasks?status=Done
+  getProjectTasksByStatus(projectId: string, status: string): Observable<TaskItem[]> {
+    return this.http.get<TaskItem[]>(
+      `${this.baseUrl}/projects/${projectId}/tasks?status=${status}`
+    );
   }
 }
